79216d919215db604fc256d4ddcbc9d9f938e184
charon4013 <charon4013@gmail.com>
2022-10-06 15:41:45 +0800

    finish day5-multi-nodes && day6-single-flight

---
+++ gee-cache/day6-single-flight/geecache/geecache.go
@@ -0,0 +1,134 @@
+package geecache
+
+import (
+	"fmt"
+	"geecache/singleflight"
+	"log"
+	"sync"
+)
+
+// Group is a cache namespace and associate data loaded spread over
+type Group struct {
+	name      string
+	getter    Getter
+	mainCache cache
+	peers     PeerPicker
+	// use singleflight.Group to make sure that each key is only fetched once
+	loader *singleflight.Group
+}
+
+var (
+	mu     sync.RWMutex
+	groups = make(map[string]*Group)
+)
+
+// 接口型函数
+
+// Getter loads data for a key
+type Getter interface {
+	Get(key string) ([]byte, error)
+}
+
+// GetterFunc implement Getter with a function
+type GetterFunc func(key string) ([]byte, error)
+
+// Get implement Getter interface function
+func (f GetterFunc) Get(key string) ([]byte, error) {
+	return f(key)
+}
+
+// NewGroup create a new instance of Group
+func NewGroup(name string, cacheBytes int64, getter Getter) *Group {
+	if getter == nil {
+		panic("nil Getter!")
+	}
+	mu.Lock()
+	defer mu.Unlock()
+
+	g := &Group{
+		name:      name,
+		getter:    getter,
+		mainCache: cache{cacheBytes: cacheBytes},
+		loader:    &singleflight.Group{},
+	}
+	groups[name] = g
+	return g
+}
+
+// GetGroup return the named group created with NewGroup
+// return nil means no such a group
+func GetGroup(name string) *Group {
+	mu.RLock()
+	g := groups[name]
+	mu.RUnlock()
+	return g
+}
+
+func (g *Group) Get(key string) (ByteView, error) {
+	if key == "" {
+		return ByteView{}, fmt.Errorf("key is required")
+	}
+
+	// if key in mainCache, then return cache data
+	if v, ok := g.mainCache.get(key); ok {
+		return v, nil
+	}
+
+	// if not, call load
+	return g.load(key)
+}
+
+// RegisterPeers registers a PeerPicker for choosing remote peer
+func (g *Group) RegisterPeers(peers PeerPicker) {
+	if g.peers != nil {
+		panic("RegisterPeerPicker called more than one")
+	}
+	g.peers = peers
+}
+
+// at distributed scene, load will first get data from getFromPeer, if failed, then it will get from getLocally
+func (g *Group) load(key string) (value ByteView, err error) {
+	// 每个key只拿一次, 不管是本地还是远程, 也不管多少次协程调用
+	// each key is only fetched once (either locally or remotely), regardless of the number of concurrent callers
+	viewi, err := g.loader.Do(key, func() (interface{}, error) {
+		if g.peers != nil {
+			// Use PickPeer to choose peer
+			if peer, ok := g.peers.PickPeer(key); ok {
+				if value, err = g.getFromPeer(peer, key); err == nil {
+					return value, nil
+				}
+				log.Println("[GeeCache] Failed to get from peer", err)
+			}
+		}
+		return g.getLocally(key)
+	})
+
+	if err != nil {
+		return viewi.(ByteView), nil
+	}
+	return
+}
+
+func (g *Group) getLocally(key string) (ByteView, error) {
+	// get source data
+	bytes, err := g.getter.Get(key)
+	if err != nil {
+		return ByteView{}, err
+	}
+	// copy to the cache
+	value := ByteView{b: cloneBytes(bytes)}
+	g.populateCache(key, value)
+	return value, nil
+}
+
+func (g *Group) getFromPeer(peer PeerGetter, key string) (ByteView, error) {
+	bytes, err := peer.Get(g.name, key)
+	if err != nil {
+		return ByteView{}, err
+	}
+	return ByteView{b: bytes}, nil
+}
+
+func (g *Group) populateCache(key string, value ByteView) {
+	g.mainCache.add(key, value)
+}
---
+++ gee-cache/day6-single-flight/go.mod
@@ -0,0 +1,11 @@
+module day5-multi-nodes
+
+go 1.19
+
+require (
+	geecache v0.0.0
+)
+
+replace (
+	geecache => ./geecache
+)
